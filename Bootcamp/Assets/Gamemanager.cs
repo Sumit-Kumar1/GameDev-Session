f7ead7ec86d3d203feccdd9731f2ccbe2e87eb71
Sumit kumar <56474834+Sumit-Kumar7796@users.noreply.github.com>
2020-12-10 18:14:42 +0530

    Update with collision script

---
+++ Bootcamp/Assets/Gamemanager.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+}
---
+++ Bootcamp/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    public Movement mmt;
+    public Rigidbody playerRigid;
+    void start()
+    {
+        playerRigid = GetComponent<Rigidbody>();
+        mmt = FindObjectOfType<Movement>();
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.tag == "Obstacle")
+        {
+           playerRigid.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
